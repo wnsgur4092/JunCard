f176bc4d4964236c9a6a2989e801e481334d82fb
Jun <wnsgur4092@gmail.com>
2022-08-20 19:28:49 +1000

    Initial Commit

---
+++ JunCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JunCard
+//
+//  Created by JunHyuk Lim on 20/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JunCard/JunCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunCardApp.swift
+//  JunCard
+//
+//  Created by JunHyuk Lim on 20/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct JunCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
